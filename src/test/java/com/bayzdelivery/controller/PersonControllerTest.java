20d0a1a389c6639d2783a0bc40e6b1ff77e48e3c
bladshed <francis.serrano@synpulse.com>
2023-07-27 20:24:21 +0800

    Modified person controller test scripts

--- src/test/java/com/bayzdelivery/controller/PersonControllerTest.java
+++ src/test/java/com/bayzdelivery/controller/PersonControllerTest.java
@@ -1,76 +1,99 @@
 package com.bayzdelivery.controller;
 
+import com.bayzdelivery.BayzDeliveryApplication;
+import com.bayzdelivery.model.LoginDto;
 import com.bayzdelivery.model.Person;
+import com.bayzdelivery.model.Role;
 import com.bayzdelivery.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(PersonController.class)
+@SpringBootTest(
+  classes = {
+    BayzDeliveryApplication.class,
+    PersonController.class
+  },
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@ActiveProfiles(value = "test")
 public class PersonControllerTest {
 
+  private LoginDto signupDto = new LoginDto(
+          "test", "password", "Test",
+          "test@yahoo.com", 'C', "123TST");
+
+  private Person person = new Person(signupDto.getUsername(), signupDto.getPassword(), signupDto.getName(),
+          signupDto.getEmail(), signupDto.getType(), signupDto.getRegistrationNumber(), new Role());
+
   @Autowired
-  MockMvc mockMvc;
+  private TestRestTemplate restTemplate;
+
+  @Autowired
+  private JwtRequestHelper jwtRequestHelper;
 
   @MockBean
   private PersonService personService;
 
   @Test
-  public void testPersonShouldBeRegistered() throws Exception {
-
-    Person person = new Person();
-    person.setId(9L);
-    person.setName("Bitoy");
-    person.setEmail("bitoy@yahoo.com");
-    person.setType('D');
-    person.setRegistrationNumber("510btyum");
-
-    when(personService.save(new Person()))
-            .thenReturn(person);
-
-    RequestBuilder request = MockMvcRequestBuilders
-            .post("/person")
-            .content("{\"name\":\"Bitoy\",\"email\":\"bitoy@yahoo.com\",\"type\":\"D\",\"registrationNumber\":\"510btyum\"}")
-            .contentType(MediaType.APPLICATION_JSON);
-
-    MvcResult result = mockMvc.perform(request)
-            .andExpect(status().isOk())
-            .andReturn();
+  public void signin() {
+    restTemplate.postForEntity("/person/signin", new LoginDto("admin", "myPass"), Void.class);
+    verify(this.personService).signin("admin","myPass");
   }
 
   @Test
-  public void getPersonById() throws Exception {
-    Person person = new Person();
+  public void allowSignupForRoleAdmin(){
+    when(personService.signup(signupDto)).thenReturn(person);
+
+    ResponseEntity<Person> response = restTemplate.exchange("/person/signup",
+            POST,
+            new HttpEntity(signupDto,jwtRequestHelper.withRole("ROLE_ADMIN")),
+            Person.class);
+
+    assertThat(response.getStatusCode().value(), is(200));
+  }
+
+  @Test
+  public void forbiddenSignupForRoleUser(){
+    when(personService.signup(signupDto)).thenReturn(person);
+
+    ResponseEntity<Person> response = restTemplate.exchange("/person/signup",
+            POST,
+            new HttpEntity(signupDto,jwtRequestHelper.withRole("ROLE_USER")),
+            Person.class);
+
+    assertThat(response.getStatusCode().value(), is(403));
+  }
+
+  @Test
+  public void testGetPersonById(){
     person.setId(1L);
-    person.setName("Kenzo");
-    person.setEmail("kenzo@yahoo.com");
-    person.setType('C');
-    person.setRegistrationNumber("777regnum");
+    when(personService.findById(1L)).thenReturn(person);
 
-    when(personService.findById(1L))
-            .thenReturn(person);
+    ResponseEntity<Person> response = restTemplate.exchange("/person/1",
+            GET,
+            new HttpEntity(null,jwtRequestHelper.withRole("ROLE_ADMIN")),
+            Person.class);
 
-    RequestBuilder request = MockMvcRequestBuilders
-            .get("/person/1")
-            .accept(MediaType.APPLICATION_JSON);
-
-    MvcResult result = mockMvc.perform(request)
-            .andExpect(status().isOk())
-            .andExpect(content().json("{\"id\":1,\"name\":\"Kenzo\",\"email\":\"kenzo@yahoo.com\",\"type\":\"C\",\"registrationNumber\":\"777regnum\"}"))
-            .andReturn();
+    assertThat(response.getStatusCode().value(), is(200));
+    assertThat(response.getBody().getId(), is(person.getId()));
+    assertThat(response.getBody().getUsername(), is(person.getUsername()));
+    assertThat(response.getBody().getName(), is(person.getName()));
   }
 
 }
